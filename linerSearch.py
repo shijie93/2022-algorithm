30e9d782b99d77b12991349633d86644a745c6f5
shijie <shijie@huya.com>
2022-01-23 09:28:05 +0800

    [查找]上传线性查找

---
+++ linerSearch.py
@@ -0,0 +1,13 @@
+# 线性查找也称为顺序查找
+# 指按顺序查找元素
+# 时间复杂度 O(N)
+
+def linear_search(li, val):
+    for idx, v in enumerate(li):
+        if v == val:
+            return idx
+    return
+
+
+print(linear_search([1, 2, 3, 4, 5, 6, 7], 3))
+print(linear_search([1, 2, 3, 4, 5, 6, 7], 8))
