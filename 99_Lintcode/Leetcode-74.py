ae5bcd645757879738b9f2558216be201c35914f
shijie <shijie@huya.com>
2022-01-25 14:31:27 +0800

    [Leetcode-74]更新

--- 99_Lintcode/Leetcode-74.py
+++ 99_Lintcode/Leetcode-74.py
@@ -14,6 +14,33 @@ class Solution:
         
         return target in matrix[hight]
 
+    def searchMatrix_2(self, matrix, target) -> bool:
+        ''' 二分法 '''
+
+        heigh = len(matrix)
+        if heigh == 0:
+            return False
+
+        width = len(matrix[0])
+
+        left = 0
+        right = heigh * width - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+            i = mid // width
+            j = mid % width
+
+            if matrix[i][j] < target:
+                left = mid + 1
+            elif matrix[i][j] > target:
+                right = mid - 1
+            else:
+                return True
+
+        return False
+
 if __name__ == '__main__':
     s = Solution()
-    print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+    print(s.searchMatrix_2([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
